4b60ab9bdf387f74f876d288d07e7959cbd31e6a
Perry Crain <pcrain9@gmail.com>
2021-07-08 10:00:48 -0500

    day 2

--- src/components/meal_stuff/MealList.js
+++ src/components/meal_stuff/MealList.js
@@ -1,45 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../layout/UI/Card';
 import Meals from './Meals';
 import classes from './MealList.module.css';
 
 function MealList() {
-
     //generate the meals
     const mealItems = [{
         id: 'm1',
         name: 'Cheeseburger',
         description: 'lorem ipsum',
-        cost: '$10'
+        cost: '10'
     },
     {
         id: 'm2',
         name: 'Hotdog',
         description: 'lorem ipsum',
-        cost: '$5'
+        cost: '5'
     },
     {
         id: 'm3',
         name: 'Macaroni salad',
         description: 'lorem ipsum',
-        cost: '$2.50'
+        cost: '2.50'
     },
     {
         id: 'm4',
         name: 'cola',
         description: 'salty',
-        cost: '$1'
+        cost: '1'
     }];
+
+    const [orderTracker, setOrderTracker] = useState([{
+        itemName:'',
+        itemCost:'',
+        total:''
+    }]);
+
+    function trackOrders(name, cost, quantity) {
+        //check for empty array
+        if(orderTracker.length === 1 && orderTracker[0].itemName===''){
+            setOrderTracker([{
+                itemName: name,
+                itemQuantity: quantity,
+                total: cost
+            }]);
+            console.log(orderTracker);
+            return;
+        }
+        //if not empty, check if key value is in array
+        for (let i of orderTracker) {
+            if (orderTracker[i].itemName === name) {
+                setOrderTracker(orderTracker[i] = {
+                    itemQuantity: quantity,
+                    total: Number(cost) * Number(quantity)
+                });
+                console.log(orderTracker);
+                return;
+            }
+        }
+
+        //if not found, create new array index and add item
+        setOrderTracker((prevOrder) => [
+            ...prevOrder,
+            {
+                itemName: name,
+                itemQuantity: quantity,
+                cost: Number(cost) * Number(quantity)
+            }
+        ]);
+        console.log(orderTracker); 
+    } 
+
+
+
 
     return (
         <Card className={classes.mealList}>
-            {mealItems.map(element=>
-                
+            {mealItems.map(element =>
                 <Meals
-                key={element.id} 
-                name={element.name}
-                description={element.description}
-                cost={element.cost} />)}
+                    key={element.id}
+                    name={element.name}
+                    description={element.description}
+                    onItemAdded={trackOrders}
+                    cost={element.cost} />)}
         </Card>
     );
 }
--- src/components/meal_stuff/Meals.js
+++ src/components/meal_stuff/Meals.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../layout/UI/Card';
 import classes from './Meals.module.css';
 
-function Meals(props){
-    return(
-        <Card className={classes.meal}>            
-                <h2>{props.name}</h2>
-                <p>{props.description}</p>
-                <h4>{props.cost}</h4>            
+function Meals(props) {
+
+    const [mealCount, setMealCount] = useState('0');
+    
+    function incrementMealsClick() {
+        setMealCount(String(Number(mealCount) + 1));
+        props.onItemAdded(props.name, props.cost, mealCount);
+    }
+    function incrementMealsType(event) {
+        setMealCount(event.target.value);
+        props.onItemAdded(props.name, props.cost, mealCount);        
+    }
+
+    return (
+        <Card className={classes.meal}>
+            <div className={classes.userInteractItems}>
+                <label htmlFor={classes.amount}>How many? </label>
+                <input type='number'
+                    min='0'
+                    max='100'
+                    id={classes.userId}
+                    placeholder={mealCount}
+                    onChange={incrementMealsType} /><br />
+                <button className={classes.addButton}
+                    onClick={incrementMealsClick}>Add+</button>
+            </div><h2>{props.name}</h2>
+            <p>{props.description}</p>
+            <h4>${props.cost}</h4>
         </Card>
     )
 }
